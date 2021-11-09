ec46b6aec7ebc4bee0132609dbc47adcded90677
Marcos Chacon <marcoschacon39@gmail.com>
2021-11-09 19:46:40 -0200

    hourglass estruturado

--- hourglass.js
+++ hourglass.js
@@ -3,17 +3,25 @@ const entry = 20
 function Hourglass(size) {
     const halfSize = Math.floor(size/2)
     const remainder = size%2
+    const sandLine = (index) => {
+        const sandLine = index === 0 ? Array(size).fill('#') : Array(size).fill(' ')
+        sandLine[index] = '#'
+        sandLine[size-index - 1] = '#'
+        
+        return sandLine.join('')
+    }
 
     this.size = size
     this.upperHalf = Array(halfSize)
     this.lowerHalf = Array(halfSize)
 
-    const sandLine = (size, length) => {
-        const sandLine = Array(size).fill(' ')
-        
-        return sandLine
+    for (let index = 0; index < halfSize; index++) {
+        this.upperHalf[index] = sandLine(index) 
+        this.lowerHalf[halfSize - index - 1] = sandLine(index)
     }
+
 }
 
 const hourglass = new Hourglass(entry)
-console.log(hourglass)
+console.log(hourglass.upperHalf)
+console.log(hourglass.lowerHalf)
