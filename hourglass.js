0a653ad433a0864f3fe5fe76953c677c71404a74
Marcos Chacon <marcoschacon39@gmail.com>
2021-11-09 18:38:49 -0200

    estrutura do Hourglass

--- hourglass.js
+++ hourglass.js
@@ -1,9 +1,18 @@
 const entry = 20
 
 function Hourglass(size) {
+    const halfSize = Math.floor(size/2)
+    const remainder = size%2
+
     this.size = size
-    this.upperHalf = Array(size/2)
-    this.lowerHalf = Array(size/2)
+    this.upperHalf = Array(halfSize)
+    this.lowerHalf = Array(halfSize)
+
+    const sandLine = (size, length) => {
+        const sandLine = Array(size).fill(' ')
+        
+        return sandLine
+    }
 }
 
 const hourglass = new Hourglass(entry)
